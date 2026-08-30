8f11ff2c116327728463d2e74df03683e344e8a7
Fnyoo <dinoibu@gmail.com>
2026-08-30 14:36:27 +0700

    Add files via upload

---
+++ Latihan13.java
@@ -0,0 +1,9 @@
+public class Latihan13 {
+
+    public static void main(String[] args) {
+        System.out.print("Halo,My name is Fauz dino");
+
+        System.out.print("\nLearning java is easy!");
+        //System.out.print("\nThe more often you practise, the better you get");
+    }    
+}
---
+++ MyFirstJava13.java
@@ -0,0 +1,7 @@
+public class MyFirstJava13 {
+
+    public static void main(String[] args) {
+        System.out.print("Halo,My Name is Fauz Dino");
+        
+    }    
+}
