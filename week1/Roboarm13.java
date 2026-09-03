f810b7fb63ebfeb7afa7a6753d955ff008f3458c
Fnyoo <dinoibu@gmail.com>
2026-09-03 23:21:46 +0700

    Add files via upload

--- week1/Roboarm13.java
+++ week1/Roboarm13.java
@@ -1,1 +1,14 @@
+public class Roboarm13 {
+    public static void main(String[] args){
 
+        System.out.println("After step 1: Tray: A = Empty, Tray B = Moon Ball, Tray C = Star Ball");
+
+        System.out.println("After step 2: Tray: A = Moon Ball, Tray B = Empty, Tray C = Star Ball");
+
+        System.out.println("After step 3: Tray: A = Moon Ball, Tray B = Star Ball, Tray C = Empty");
+
+        System.out.println("Conclusion: The correct conclusion is (a) the two balls have changed places and (e) tray C is empty");
+
+    }
+
+}
